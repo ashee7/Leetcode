e8891b30bc9d1d68d21962210b81f6b9c305614e
Ashee Mahajan <59741112+ashee7@users.noreply.github.com>
2025-06-19 00:30:07 +0530

    Time: 74 ms (88.61%), Space: 32.8 MB (90.3%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.py
@@ -1,19 +1,25 @@
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
         sorted_nums=sorted(nums)
-        arr=[]
-        dif=0
+        # arr=[]
+        # dif=0
+        # res=[]
+        # totaldif=0
+        # for i in range(1,len(sorted_nums)+1):
+        #     arr.append(sorted_nums[i-1])
+        #     if i%3==0:
+        #         if totaldif>k:
+        #             return []
+        #         res.append(arr)
+        #         arr=[]
+        #         totaldif=0
+        #         continue
+        #     dif=sorted_nums[i]-sorted_nums[i-1]
+        #     totaldif+=dif
+        # return res
         res=[]
-        totaldif=0
-        for i in range(1,len(sorted_nums)+1):
-            arr.append(sorted_nums[i-1])
-            if i%3==0:
-                if totaldif>k:
-                    return []
-                res.append(arr)
-                arr=[]
-                totaldif=0
-                continue
-            dif=sorted_nums[i]-sorted_nums[i-1]
-            totaldif+=dif
+        for i in range(0,len(nums),3):
+            if (sorted_nums[i+2]-sorted_nums[i])>k:
+                return []
+            res.append(sorted_nums[i:i+3])
         return res
