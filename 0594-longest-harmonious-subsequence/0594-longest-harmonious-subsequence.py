4c149d08e3b6a7dc503dae4aa0a867fb011af203
Ashee Mahajan <59741112+ashee7@users.noreply.github.com>
2025-06-30 20:35:31 +0530

    Time: 64 ms (11.73%), Space: 18.7 MB (98.43%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,13 +1,25 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        counts=Counter(nums)
-        countskeys=sorted(counts.keys())
-        total=0
-        for i in range(len(countskeys)-1):
-            mini=countskeys[i]
-            maxi=countskeys[i+1]
+        sortnums=sorted(nums)
 
-            if maxi-mini==1:
-                total=max(total,counts[mini]+counts[maxi])
+        # countskeys=sorted(counts.keys())
+        # total=0
+        # for i in range(len(countskeys)-1):
+        #     mini=countskeys[i]
+        #     maxi=countskeys[i+1]
+
+        #     if maxi-mini==1:
+        #         total=max(total,counts[mini]+counts[maxi])
                 
+        # return total
+        
+        total=0
+        i,j=0,0
+        while i <len(sortnums):
+            if sortnums[i]-sortnums[j]>1:
+                j+=1
+            else:
+                if sortnums[i]-sortnums[j]==1:
+                    total=max(total,i-j+1)
+                i+=1
         return total
