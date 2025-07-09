4cfb751e61cc5702cd00de3c10e7e800e22beea4
Ashee Mahajan <59741112+ashee7@users.noreply.github.com>
2025-07-09 22:01:41 +0530

    Time: 0 ms (100%), Space: 17.7 MB (78.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        s=list()
+        s.append(1)# base case when 1 step only
+        s.append(2)# base case when 2 steps
+        if n<2:
+            return s[n-1]
+        for _ in range(3,n+1):
+            s.append(s[-1]+s[-2])
+        
+        return s[-1]
+        
